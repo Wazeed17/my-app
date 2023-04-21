fe75cf74842c245d2a6292471bff8d80de559f35
Wazeed17 <shaik.wazeed17@gmail.com>
2023-04-22 00:01:46 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './components/Header'
+
+const App = () => {
+  return (
+    <>
+    <h1>App</h1>
+   <Header/>
+    </>
+  )
+}
+
+export default App
